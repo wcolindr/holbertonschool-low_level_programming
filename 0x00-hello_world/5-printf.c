4410442b407d3b1e52616c57c68d6deda56c72f7
William Colindres <1689@holbertonschool.com>
2020-09-16 16:21:56 +0000

    print a string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
