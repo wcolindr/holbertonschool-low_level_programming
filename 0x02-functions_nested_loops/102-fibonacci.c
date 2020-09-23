fc4a145416e482f69c3a88401c0593b73aefd963
William Colindres <1689@holbertonschool.com>
2020-09-23 18:15:36 +0000

    print 50 fibonacci starting with 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int a, num1 = 1, num2 = 2, nextNum;
+
+	for (a = 1; a <= 50; a++)
+	{
+		printf("%ld, ", num1);
+		nextNum = num1 + num2;
+		num1 = num2;
+		num2 = nextNum;
+
+		if (a == 50)
+		{
+			printf("%ld", num1);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
