6dcc697b37417080ce636fd70fffd5e29d7de5ee
William Colindres <1689@holbertonschool.com>
2020-09-23 18:47:51 +0000

    sum all even fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int sum, num1 = 1, num2 = 2, evenNum = 2;
 
-	for (;num1 + num2 < 4000000;)
+	for (; num1 + num2 < 4000000;)
 	{
 		sum = num1 + num2;
 		num1 = num2;
@@ -18,7 +18,7 @@ int main(void)
 
 		if (sum % 2 == 0)
 		{
-			evenNum = sum;
+			evenNum += sum;
 		}
 	}
 	printf("%d\n", evenNum);
