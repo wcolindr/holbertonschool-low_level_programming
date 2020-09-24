51d56275b06309c3735944dc4bb7ccd1805a8070
William Colindres <1689@holbertonschool.com>
2020-09-24 18:07:04 +0000

    Print fizz buzz for every 3, 5, and 15 from numbers 1 to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print fizzbuzz on numbers 1 to 100
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
