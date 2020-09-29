132d703db82383468ae58ad851e670d63df5bb14
William Colindres <1689@holbertonschool.com>
2020-09-29 17:42:19 +0000

    function that copies a string to another string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,13 +12,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	unsigned char i;
+	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	dest[i] |= '\0';
+	dest[i] = '\0';
 	return (dest);
 }
