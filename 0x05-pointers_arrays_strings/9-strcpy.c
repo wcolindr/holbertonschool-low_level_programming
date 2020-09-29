d7bacc9c09d7dc9c080bcd127c79faab3d5b706c
William Colindres <1689@holbertonschool.com>
2020-09-29 16:53:50 +0000

    function that copies a string to another string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copy a string to another string
+ *
+ * @dest: char is dest
+ *
+ * @src: char is src
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	unsigned char i;
+
+	for (i = 0; src[i] != '\0'; ++i)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
