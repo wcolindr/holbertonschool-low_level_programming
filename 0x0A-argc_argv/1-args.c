09753772f1be7d1446d4bde094dc0b72707be6b9
William Colindres <1689@holbertonschool.com>
2020-10-09 18:56:35 +0000

    program that prints the total number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: int is argc
+ *
+ * @argv: char is argv
+ *
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	int counter;
+	int sum = 0;
+
+	for(counter = 0; counter < argc; counter++)
+		sum = sum + *argv[counter];
+
+	printf("%d\n",counter - 1);
+	
+	return (0);
+}
