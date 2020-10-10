ee59ceaadf4e8d77d9e3a7ddcc2a6a9b9a1eb71b
William Colindres <1689@holbertonschool.com>
2020-10-09 23:56:40 -0400

    program that multiplies two integers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * 
+ * @argc: int is argc
+ * 
+ * @argv: char is argv
+ * 
+ * Return: 0 and 1
+ */
+
+int main(int argc, char *argv[])
+{
+    int a,b,mul;
+	if(argc != 2)
+	{
+		printf("Error\n");
+		return 1;
+    }
+	mul = a * b;
+	
+	printf("Production of %d, %d is: %d\n",a,b,mul);
+	
+	return 0;
+
+}
