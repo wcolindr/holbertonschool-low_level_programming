dba025ec7e85678388b63709412c45827869fc7c
William Colindres <1689@holbertonschool.com>
2020-10-11 04:50:17 +0000

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: int is argv
+ *
+ * @argv: char is argv
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
