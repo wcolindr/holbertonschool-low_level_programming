102453ddc68f366238f65585dda6e34778fa110c
William Colindres <1689@holbertonschool.com>
2020-10-19 22:54:05 +0000

    program that prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
+/**
+ * filename - print file name that was compiled from
+ */
+
 void filename(void)
 {
 	printf("%s\n", __FILE__);
 }
 
-int main(int argc, char ** argv)
+/**
+ * main - main function
+ *
+ * @argc: int is argc
+ *
+ * @argv: char is argv
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
 {
 	(void) argc;
-	(void) ** argv;
+	(void) **argv;
 	filename();
-	return 0;
+	return (0);
 }
