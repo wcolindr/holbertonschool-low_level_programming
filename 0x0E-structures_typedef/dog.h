3812e5527e592f9131c7cf9c01146f59024a2c72
William Colindres <1689@holbertonschool.com>
2020-10-20 01:07:29 +0000

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * struct dog - new type for my_dog
+ *
+ * @name: name of dog
+ *
+ * @age: age of dog
+ *
+ * @owner: owner of dog
+ */
 typedef struct dog
 {
 	char *name;
