780a75248e1b1854d06f0e6943a384fad0c00722
William Colindres <1689@holbertonschool.com>
2020-10-20 17:51:37 +0000

    header file for dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,14 @@ struct dog
 
 typedef struct dog dog_t;
 
+int _putchar(int c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
