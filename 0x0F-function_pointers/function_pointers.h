484678737bc68098b397d6dac630932b3c5e11fc
William Colindres <1689@holbertonschool.com>
2020-10-21 17:10:59 +0000

    prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(int c);
 
 void print_name(char *name, void (*f)(char *));
@@ -5,3 +8,5 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, int size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
