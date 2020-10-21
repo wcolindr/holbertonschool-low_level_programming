74014c479f22a26239e92d3e46e84674542d8f45
William Colindres <1689@holbertonschool.com>
2020-10-21 17:05:44 +0000

    prototypes

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+int _putchar(int c);
+
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, int size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
