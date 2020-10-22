2248906b2134f1ef4a236df7d179cee0102e95ee
William Colindres <1689@holbertonschool.com>
2020-10-22 00:31:44 +0000

    function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action(array[i]) != NULL)
+	if (action != NULL && array != NULL)
 	{
 	for (i = 0; i < size; i++)
 		action(array[i]);
