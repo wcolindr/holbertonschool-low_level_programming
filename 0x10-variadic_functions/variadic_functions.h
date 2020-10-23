ab6f06d9d057ef51dbedcac73636228b8ff97a03
William Colindres <1689@holbertonschool.com>
2020-10-23 04:56:13 +0000

    prototypes

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+#endif
