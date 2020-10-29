014ad10419876dac355ebe6c0daf0fde7d395b49
William Colindres <1689@holbertonschool.com>
2020-10-29 19:54:28 +0000

    prototypes and struct for list_t

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,13 @@
 
 #include <stdlib.h>
 
+typedef struct list_s
+{
+        char *str;
+        unsigned int len;
+        struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 
 size_t print_list(const list_t *h);
