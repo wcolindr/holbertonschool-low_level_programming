a05f54d08ceb0ff108267ae938af647914ce09b3
William Colindres <1689@holbertonschool.com>
2020-10-30 19:01:52 +0000

    64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,42 +1,21 @@
-	.file	"test.c"
-	.section	.rodata
-	.LC0:
-	.string	"%s"
-	.text
-	.globl	main
-	.type	main, @function
+        extern	printf
+
+	        section .data
+msg:		db "Hello, Holberton", 0
+fmt:	    db "%s", 10, 0
+
+	        section .text
+
+	        global main
 main:
-	.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movq	%fs:40, %rax
-	movq	%rax, -8(%rbp)
-	xorl	%eax, %eax
-	movabsq	$5197202827057587528, %rax
-	movq	%rax, -32(%rbp)
-	movabsq	$7957707101262998639, %rax
-	movq	%rax, -24(%rbp)
-	movb	$0, -16(%rbp)
-	leaq	-32(%rbp), %rax
-	movq	%rax, %rsi
-	movl	$.LC0, %edi
-	movl	$0, %eax
-	call	printf
-	movq	-8(%rbp), %rdx
-	xorq	%fs:40, %rdx
-	je	.L2
-	call	__stack_chk_fail
-	.L2:
-	leave
-	.cfi_def_cfa 7, 8
+	        push    rbp
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		
+	        call    printf
+
+	pop	rbp
+
+	mov	rax,0
 	ret
-	.cfi_endproc
-	.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
-	.section	.note.GNU-stack,"",@progbits
