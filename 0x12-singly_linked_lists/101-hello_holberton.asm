79f43512cb35d6ad3872a435be863351e99534b6
William Colindres <1689@holbertonschool.com>
2020-10-30 15:18:49 +0000

    64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,11 @@
-	.file	"test.c"
-	.section	.rodata
-	.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+.LC0:
+	        .string "Hello, Holberton"
 main:
-	.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	movl	$0, %eax
-	call	printf
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-	.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
-	.section	.note.GNU-stack,"",@progbits
+	        push    rbp
+	        mov     rbp, rsp
+	        mov     edi, OFFSET FLAT:.LC0
+	        mov     eax, 0
+	        call    printf
+	        mov     eax, 0
+	        pop     rbp
+	        ret
