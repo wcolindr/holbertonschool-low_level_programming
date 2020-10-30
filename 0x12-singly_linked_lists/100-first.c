e734263aa8ff72562075df61b2af54aab1606f1c
William Colindres <1689@holbertonschool.com>
2020-10-30 14:01:43 +0000

    functions that prints a string before executing the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@
  * premain - execute before main
  */
 
-void premain()
+void premain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
